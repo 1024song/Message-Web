ea082bba601060fe56a0f03a5b4a25cfc50e554b
1024song <1164453353@qq.com>
2019-03-20 15:14:04 +0800

    发送message 异步

--- src/main/java/com/mysql/async/handler/LikeHandler.java
+++ src/main/java/com/mysql/async/handler/LikeHandler.java
@@ -3,17 +3,42 @@ package com.mysql.async.handler;
 import com.mysql.async.EventHander;
 import com.mysql.async.EventModel;
 import com.mysql.async.EventType;
+import com.mysql.model.Message;
+import com.mysql.model.User;
+import com.mysql.service.MessageService;
+import com.mysql.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 @Component
 public class LikeHandler implements EventHander {
 
+    @Autowired
+    MessageService messageService;
+
+    @Autowired
+    UserService userService;
+
     @Override
     public void doHandle(EventModel model) {
-        System.out.println("Liked");
+        //System.out.println("Liked");
+        Message message = new Message();
+        message.setFromId(3);
+        message.setToId(model.getEntityOwnerId());
+        User user = userService.getUser(model.getActorId());
+        message.setContent("用户" + user.getName()
+                + "赞了你的资讯，http://127.0.0.1:8080/news/" + String.valueOf(model.getEntityId()));
+        message.setCreatedDate(new Date());
+        int fromId = message.getFromId();
+        int toId = message.getToId();
+        message.setConversationId(fromId < toId ? String.format("%d_%d", fromId, toId) :
+                String.format("%d_%d", toId, fromId));
+        //message.setConversationId();
+        messageService.addMessage(message);
     }
 
     @Override
